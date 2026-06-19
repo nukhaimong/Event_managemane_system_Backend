2f19b56282f2629870de9b22ad7106e73118e2ef
nukhaimong <nukhaimongmarma834@gmail.com>
2026-06-19 22:33:13 +0600

    initial project setup with golang framwork Echo

---
+++ cmd/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+type User struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	if err := cv.validator.Struct(i); err != nil {
+		// Optionally return the error to let each route control the status code.
+		return echo.ErrBadRequest.Wrap(err)
+	}
+	return nil
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	e.GET("/", func(c *echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, World!"})
+	})
+	e.GET("/jekono", func(c *echo.Context) error {
+		return c.JSON(http.StatusOK, "Hello From Jekono!")
+	})
+	e.POST("/users", func(c *echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]any{"error": err.Error()})
+		}
+		if err := c.Validate(u); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]any{"error": err.Error()})
+		}
+		return c.JSON(http.StatusCreated, u)
+	})
+
+	if err := e.Start(":8000"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
